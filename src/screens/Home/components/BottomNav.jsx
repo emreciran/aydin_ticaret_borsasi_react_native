2e952efa2ed670c0c2e93ec186ce25baface8b3b
emreciran <otogaming09@gmail.com>
2023-03-14 12:24:17 +0300

    bottom nav updated

--- src/screens/Home/components/BottomNav.jsx
+++ src/screens/Home/components/BottomNav.jsx
@@ -12,65 +12,71 @@ const BottomNav = () => {
   return (
     <View style={styles.container}>
       <View style={styles.group}>
-        <TouchableOpacity style={styles.circle} onPress={() => navigation.navigate("Kurumsal")}>
-          <MaterialIcons name="work-outline" size={28} color="#f0f0f0" />
+        <TouchableOpacity
+          style={styles.circle}
+          onPress={() => navigation.navigate("Kurumsal")}
+        >
+          <MaterialIcons name="work-outline" size={28} color="#fff" />
         </TouchableOpacity>
         <Text>Kurumsal</Text>
       </View>
       <View style={styles.group}>
         <TouchableOpacity style={styles.circle}>
-          <Ionicons name="megaphone-outline" size={28} color="#f0f0f0" />
+          <Ionicons name="megaphone-outline" size={28} color="#fff" />
         </TouchableOpacity>
         <Text>Duyuru</Text>
       </View>
       <View style={styles.group}>
         <TouchableOpacity style={styles.circle}>
-          <Ionicons name="earth-outline" size={28} color="#f0f0f0" />
+          <Ionicons name="earth-outline" size={28} color="#fff" />
         </TouchableOpacity>
         <Text>Haber</Text>
       </View>
       <View style={styles.group}>
         <TouchableOpacity style={styles.circle}>
-          <Ionicons name="newspaper-outline" size={28} color="#f0f0f0" />
+          <Ionicons name="newspaper-outline" size={28} color="#fff" />
         </TouchableOpacity>
         <Text>Bülten</Text>
       </View>
       <View style={styles.group}>
         <TouchableOpacity style={styles.circle}>
-          <Ionicons name="card-outline" size={28} color="#f0f0f0" />
+          <Ionicons name="card-outline" size={28} color="#fff" />
         </TouchableOpacity>
         <Text>Aidat/Üye</Text>
       </View>
-      <View style={styles.group}>
-        <TouchableOpacity style={styles.circle}>
-          <FontAwesome name="exchange" size={28} color="#f0f0f0" />
-        </TouchableOpacity>
-        <Text>Piyasa</Text>
-      </View>
-      <View style={styles.group}>
-        <TouchableOpacity style={styles.circle}>
-          <Ionicons name="calendar-outline" size={28} color="#f0f0f0" />
-        </TouchableOpacity>
-        <Text>Etkinlik</Text>
-        <Text>Takvimi</Text>
-      </View>
-      <View style={styles.group}>
-        <TouchableOpacity style={styles.circle}>
-          <FontAwesome name="pencil-square" size={26} color="#f0f0f0" />
-        </TouchableOpacity>
-        <Text>Talep Öneri</Text>
-      </View>
-      <View style={styles.group}>
-        <TouchableOpacity style={styles.circle}>
-          <Ionicons name="call-outline" size={28} color="#f0f0f0" />
-        </TouchableOpacity>
-        <Text>İletişim</Text>
-      </View>
-      <View style={styles.group}>
-        <TouchableOpacity style={styles.circle}>
-          <Ionicons name="settings-outline" size={28} color="#f0f0f0" />
-        </TouchableOpacity>
-        <Text>Ayarlar</Text>
+      <View style={{flexDirection: "row", gap: 18}}>
+        <View style={styles.group}>
+          <TouchableOpacity style={styles.circle}>
+            <FontAwesome name="exchange" size={28} color="#fff" />
+          </TouchableOpacity>
+          <Text>Piyasa</Text>
+        </View>
+        <View style={styles.group}>
+          <TouchableOpacity style={styles.circle}>
+            <Ionicons name="calendar-outline" size={28} color="#fff" />
+          </TouchableOpacity>
+          <Text>Etkinlik</Text>
+          <Text>Takvimi</Text>
+        </View>
+        <View style={styles.group}>
+          <TouchableOpacity style={styles.circle}>
+            <FontAwesome name="pencil-square" size={26} color="#fff" />
+          </TouchableOpacity>
+          <Text>Talep</Text>
+          <Text>Öneri</Text>
+        </View>
+        <View style={styles.group}>
+          <TouchableOpacity style={styles.circle}>
+            <Ionicons name="call-outline" size={28} color="#fff" />
+          </TouchableOpacity>
+          <Text>İletişim</Text>
+        </View>
+        <View style={styles.group}>
+          <TouchableOpacity style={styles.circle}>
+            <Ionicons name="settings-outline" size={28} color="#fff" />
+          </TouchableOpacity>
+          <Text>Ayarlar</Text>
+        </View>
       </View>
     </View>
   );
--- src/screens/Home/components/SocialLinks.jsx
+++ src/screens/Home/components/SocialLinks.jsx
@@ -2,17 +2,18 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import styles from "../styles/SocialLinks";
+import * as WebBrowser from "expo-web-browser"
 
 const SocialLinks = () => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={async () => WebBrowser.openBrowserAsync("https://tr-tr.facebook.com/")}>
         <Ionicons name="logo-facebook" size={30} color="#4267B2" />
       </TouchableOpacity>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={async () => WebBrowser.openBrowserAsync("https://twitter.com/")}>
         <Ionicons name="logo-twitter" size={30} color="#1DA1F2" />
       </TouchableOpacity>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={async () => WebBrowser.openBrowserAsync("https://www.instagram.com/")}>
         <Ionicons name="logo-instagram" size={30} color="#C13584" />
       </TouchableOpacity>
     </View>
